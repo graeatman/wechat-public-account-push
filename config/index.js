bb84698bccf750c779056e26f65551750261a561
graeatman <114131273+graeatman@users.noreply.github.com>
2022-09-22 14:23:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "宝贝",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",wes_N0wNrdnaqU5fpHWQGUbfOXXS68pJc_KlNAinp-k
+      useTemplateId: "KiITa_wnWnva2705p4SMcJHiqIkTbcmuRj93rhaNZH0",
       // 所在省份
       province: "河南",
       // 所在城市
