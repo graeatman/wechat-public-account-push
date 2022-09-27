b499cb1b8556faf67e44d942b6077b8a11c8276d
graeatman <114131273+graeatman@users.noreply.github.com>
2022-09-27 21:56:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,19 +5,19 @@ export const config = {
     // 公众号appSecret
     appSecret: "f57dd857fcaadc13ea60c42b06122598",
     // 模板消息id
-    templateId: "	MedC8qBzsTBp3mfg5sM7XEhnvHEN2TKPvqxRf3KHaT8	",
+    templateId: "MedC8qBzsTBp3mfg5sM7XEhnvHEN2TKPvqxRf3KHaT8",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: [oNona50juWkSsMAczhkJ9SZ5ueWE，oNona58Dg4RCYZSjcZBTZXUr_vPI],
 
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "河南",
     // 所在城市
-    city: "惠州",
+    city: "郑州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "宝贝", "year": "2004", "date": "04-24", "type": 'new'},
-      {"name": "家公", "year": "2004", "date": "02-07", "type": 'new'},
+      {"name": "二臂", "year": "2004", "date": "02-07", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2022-06-10",
